18ab6c0d41b6c3098f698e742a7424f3f16447f0
Meletis Flevarakis <fmeletis85@gmail.com>
2019-03-18 18:32:37 +0000

    Apply fixes from StyleCI

--- src/Events/CheckFailed.php
+++ src/Events/CheckFailed.php
@@ -6,16 +6,15 @@ use Meletisf\ZenDoctor\Interfaces\HealthCheckInterface;
 
 class CheckFailed
 {
-
     protected $check;
 
     /**
      * CheckFailed constructor.
+     *
      * @param HealthCheckInterface $check
      */
-    function __construct(HealthCheckInterface $check)
+    public function __construct(HealthCheckInterface $check)
     {
         $this->check = $check;
     }
-
 }
--- src/Facades/ZenDoctor.php
+++ src/Facades/ZenDoctor.php
@@ -6,10 +6,8 @@ use Illuminate\Support\Facades\Facade;
 
 class ZenDoctor extends Facade
 {
-
     protected static function getFacadeAccessor()
     {
         return 'zen-doctor';
     }
-
 }
